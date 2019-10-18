2306f21cb3858e4cbbccb9189d111eb883fb7e30
Christine Frank <christine.braun13@gmail.com>
2019-10-18 09:46:47 -0700

    Add create routing and view for stylist, add detail view and files for edit and delete views

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+    public class StylistsController : Controller
+    {
+        private readonly HairSalonContext _db;
+
+        public StylistsController(HairSalonContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Stylist> model = _db.Stylists.ToList();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Stylist stylist)
+        {
+            _db.Stylists.Add(stylist);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            Stylist thisStylist = _db.Stylists.Include(stylist => stylist.Clients).FirstOrDefault();
+            return View(thisStylist);
+        }
+    }
+}
