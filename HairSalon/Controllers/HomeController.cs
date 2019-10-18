4878c37be73f70e9a8eba344fb1c7afae86be216
Christine Frank <christine.braun13@gmail.com>
2019-10-18 08:51:08 -0700

    Add initial files from new mvc command. Remove unnessessary content and add initial structure to run skeleton Home page

---
+++ HairSalon/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
