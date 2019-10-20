afbaeb6c05857a517cc219ae816036563990f264
Christine Frank <christine.braun13@gmail.com>
2019-10-20 15:21:27 -0700

    Add search side bar to layout. Add search results view and route in home

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -10,9 +10,33 @@ namespace HairSalon.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly HairSalonContext _db;
+
+        public HomeController(HairSalonContext db)
+        {
+            _db = db;
+        }
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost("/Search")]
+        public ActionResult Search(string searchType, string searchInput)
+        {
+            ViewBag.SearchType = searchType;
+            ViewBag.searchInput = searchInput;
+            List<Object> model;
+            if(searchType == "stylist")
+            {
+                model = _db.Stylists.Where(stylist => stylist.FirstName.ToLower().Contains(searchInput.ToLower()) || stylist.LastName.ToLower().Contains(searchInput.ToLower()) || searchInput.ToLower().Contains(stylist.FirstName.ToLower()) || searchInput.ToLower().Contains(stylist.LastName.ToLower())).Select(s=>(Object) s).ToList();
+            }
+            else
+            {
+                model = _db.Clients.Where(client => client.FirstName.ToLower().Contains(searchInput.ToLower()) || client.LastName.ToLower().Contains(searchInput.ToLower()) || searchInput.ToLower().Contains(client.FirstName.ToLower()) || searchInput.ToLower().Contains(client.LastName.ToLower())).Select(s => (Object) s).ToList();
+            }
+            return View(model);
         }
     }
 }
